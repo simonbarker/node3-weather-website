98c1d28e1de6cc7333ebc7a93cf5afda8f2a8fd0
Simon Barker <simonbarker@MacBook-Pro-540.local>
2019-09-02 16:31:56 +0100

    Add wind speed and gust to summary display

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,8 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback(body.error)
         } else {
-            callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability}% chance of rain.`)
+            console.log(body)
+            callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out with wind gusting to ${body.currently.windGust} mph from a speed of ${body.currently.windSpeed} mph. There is a ${body.currently.precipProbability}% chance of rain.`)
         }
 
     })
